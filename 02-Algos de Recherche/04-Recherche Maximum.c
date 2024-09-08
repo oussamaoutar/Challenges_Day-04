77489eca7adc699f49bd1412e200422122fb8a8a
oussama <outaroussama88@gmail.com>
2024-09-09 00:17:35 +0100

    Modification sur la fonction de recherche

--- 02-Algos de Recherche/04-Recherche Maximum.c
+++ 02-Algos de Recherche/04-Recherche Maximum.c
@@ -4,15 +4,19 @@
 // Fonction de recherche linéaire pour trouver le maximum du tableau
 int rechLineaire(int T[], int taille) {
     int max = T[0];
-    for(int i=0; i<taille; i++) if(T[i]>max) max = T[i];
-    return max;
+    int indexMax = 0;
+    for(int i=0; i<taille; i++) if(T[i]>max) {
+        max = T[i];
+        indexMax = i;
+    } 
+    printf("Le maximum est: %d\nIndice: %d", max, indexMax);
 } 
 
 int main() {
 
     int T[] = {5, 3, 43, -1, 0};
     int taille = (sizeof(T) / sizeof(int)); // Calcul de la taille du tableau T
-    printf("Le maximum est: %d", rechLineaire(T, taille));
+    rechLineaire(T, taille);
 
     return 0;
 }
