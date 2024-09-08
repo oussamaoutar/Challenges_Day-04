eb89d8a2987af4390b765afc2ab595e2c3ad26e1
oussama <outaroussama88@gmail.com>
2024-09-09 00:15:26 +0100

    Modification sur la fonction de recherche

--- 02-Algos de Recherche/03-Recherche Minimum.c
+++ 02-Algos de Recherche/03-Recherche Minimum.c
@@ -4,15 +4,19 @@
 // Fonction de recherche linéaire pour trouver le minimum du tableau
 int rechLineaire(int T[], int taille) {
     int min = T[0];
-    for(int i=0; i<taille; i++) if(T[i]<min) min = T[i];
-    return min;
+    int indexMin = 0;
+    for(int i=0; i<taille; i++) if(T[i]<min) {
+        min = T[i];
+        indexMin = i;
+    } 
+    printf("Le minimum est: %d\nIndice: %d", min, indexMin);
 } 
 
 int main() {
 
     int T[] = {5, 3, 43, -1, 0};
     int taille = (sizeof(T) / sizeof(int)); // Calcul de la taille du tableau T
-    printf("Le minimum est: %d", rechLineaire(T, taille));
+    rechLineaire(T, taille);
 
     return 0;
 }
